b4cd107e37fcb4d9ed50c0250c37adc8e8fa23e4
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2020-10-18 21:55:31 +0530

    stack | tree

---
+++ Medium/144-Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,36 @@
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+
+        if(root==NULL)return res;
+
+        stack<TreeNode*> s;
+        s.push(root);
+
+        while(!s.empty()){
+            auto node = s.top();
+            s.pop();
+            res.push_back(node->val);
+            if(node->right)
+                s.push(node->right);
+            if(node->left)
+                s.push(node->left);
+        }
+        
+        return res;
+    }
+};
