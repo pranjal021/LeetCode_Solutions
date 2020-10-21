9d497a84369248687493086c1577485d263dd2e8
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2020-10-21 15:32:56 +0530

    stack

--- Medium/735-Asteroid Collision.cpp
+++ Medium/735-Asteroid Collision.cpp
@@ -1,3 +1,5 @@
+//Method 1:
+
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
@@ -12,5 +14,53 @@ public:
             if(!destroyed) res.push_back(a);
         }
         return res;
+    }
+};
+
+//Method 2: using STACK
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> s;
+        
+        for(int i=0;i<asteroids.size();i++){
+            
+            if(s.empty() || asteroids[i]>0){
+                s.push(asteroids[i]);
+            }else{
+                
+                while(true){
+                    int top = s.top();
+                    
+                    if(top<0){
+                        s.push(asteroids[i]);
+                        break;
+                    }else if(top == -asteroids[i]){
+                        s.pop();
+                        break;
+                    }else if(top > (-asteroids[i])){
+                        break;
+                    }else{
+                        s.pop();
+                        if(s.empty()){
+                            s.push(asteroids[i]);
+                            break;
+                        }
+                    }
+                }
+                
+            }
+            
+        }
+        
+        
+        vector<int> res(s.size(),0);
+        for(int i=s.size()-1;i>=0;i--){
+            res[i] = s.top();
+            s.pop();
+        }
+        return res;
+        
     }
 };
