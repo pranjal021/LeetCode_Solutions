1e10b9280725b649e6d440efd8549b4ec33bb4ee
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2020-11-02 00:48:16 +0530

    bit-manipulation

---
+++ Easy/190-Reverse Bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0;
+        for (int i = 0; i < 31; i++) {
+            res = (n % 2) + res << 1;
+            n >>= 1;
+        }
+        return res + n % 2;
+    }
+};
+
