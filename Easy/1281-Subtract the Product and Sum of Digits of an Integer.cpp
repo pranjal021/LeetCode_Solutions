29081dd0ce279f81c847c2f03b49c5fb7975d0db
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2020-11-13 16:10:30 +0530

    math

---
+++ Easy/1281-Subtract the Product and Sum of Digits of an Integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        
+        int sum = 0;
+        int product = 1;
+        
+        while(n){
+            sum += n%10;
+            product *= n%10;
+            n = n/10;
+        }
+        
+        return product-sum;
+    }
+};
