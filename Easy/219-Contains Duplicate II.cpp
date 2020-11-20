5b32a910678830939b2adc7f25be43691b571da8
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2020-11-20 20:14:00 +0530

    array | hashtable

---
+++ Easy/219-Contains Duplicate II.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        
+        unordered_map<int,int> m;
+        
+        for(int i=0;i<nums.size();i++){
+            
+            if(!m.count(nums[i])){
+                
+                m[nums[i]] = i;
+                
+            }else{
+                
+                if(abs(i - m[nums[i]]) <= k)
+                    return true;
+                else
+                    m[nums[i]] = i;
+                
+            }    
+            
+        }
+        
+        return false;
+    }
+};
