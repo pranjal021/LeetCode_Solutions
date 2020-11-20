7e60bfe58143daf52480aebb2419ed84734f4a11
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2020-11-21 00:05:36 +0530

    bit manipulation

---
+++ Easy/342. Power of Four.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==0) return false;
+        
+        while(n!=1){
+            if(n % 4 != 0)return false;
+            n = n/4;
+        }
+        
+        return n==1;
+    }
+};
