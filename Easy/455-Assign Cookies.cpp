5b631f2571b641a35a79b725153e6f03e05a1916
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2020-11-24 20:28:34 +0530

    greedy

---
+++ Easy/455-Assign Cookies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+       
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+
+        int j = 0;
+        int i = 0;
+        while(i < g.size() && j < s.size()){
+            if(s[j] >= g[i])
+                i++;
+            
+            j++;
+        }
+        
+        return i;
+    }
+};
