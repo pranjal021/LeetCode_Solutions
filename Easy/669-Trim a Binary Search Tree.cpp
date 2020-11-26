5d9122d85950fc7f4988adbf54bdb973617eb0b5
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2020-11-26 12:25:57 +0530

    tree

---
+++ Easy/669-Trim a Binary Search Tree.cpp
@@ -0,0 +1,33 @@
+//Method 1:
+
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        
+        if(!root) return NULL;
+        if(root->val < low) return trimBST(root->right,low,high);
+        if(root->val > high) return trimBST(root->left,low,high);
+        root->left = trimBST(root->left,low,high);
+        root->right = trimBST(root->right,low,high);
+        return root;
+        
+    }
+};
+
+//Method 2:
+
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if(!root) return root;
+        if(root->val >= low && root->val <= high){
+            root->left = trimBST(root->left,low,high);
+            root->right = trimBST(root->right,low,high);
+            return root;
+        }
+        
+        if(root->val < low)
+            return trimBST(root->right,low,high);
+        return trimBST(root->left,low,high);
+    }
+};
