771192bdca20232af8ea133d768ae547d46e48ca
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2020-12-01 21:53:57 +0530

    math | string

---
+++ Medium/12-Integer to Roman.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        
+        string res;
+        string sym[] = {"M", "CM" , "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int val[] =    {1000, 900,  500,  400, 100,  90,   50,  40,  10,9,5,4,1};
+        
+        for(int i=0; num!=0; i++){
+            
+            while(num >= val[i]){
+                num -= val[i];
+                res += sym[i];
+            }
+            
+        }
+        
+        return res;
+    }
+};
