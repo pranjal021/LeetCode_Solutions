b0c240b7ecb7a42c3bd8c492b71afd5d110171d6
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2020-12-07 11:53:16 +0530

    tree | dfs

---
+++ Medium/117-Populating Next Right Pointers in Each Node II.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root) return root;
+        
+        queue<Node*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int s = q.size();
+            
+            while(s--){
+                auto n = q.front();
+                q.pop();
+                
+                if(!q.empty() && s){
+                    n->next = q.front();
+                }
+                
+                if(n->left) q.push(n->left);
+                if(n->right) q.push(n->right);
+            }
+                    
+        }
+        
+        return root;
+    }
+};
