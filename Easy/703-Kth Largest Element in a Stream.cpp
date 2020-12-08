470d816963f16c81d91dc43889499e2093c05af7
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2020-12-08 18:06:03 +0530

    heap | design

---
+++ Easy/703-Kth Largest Element in a Stream.cpp
@@ -0,0 +1,26 @@
+class KthLargest {
+public:
+    
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int digit;
+    
+    KthLargest(int k, vector<int>& nums) {
+        digit = k;
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+            if(pq.size()>k) pq.pop();
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size()>digit) pq.pop();
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
