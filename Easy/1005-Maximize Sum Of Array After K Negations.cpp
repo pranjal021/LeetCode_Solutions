35f323791d058b3e20c259033ad5800d57a454c7
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2020-12-10 12:13:45 +0530

    greedy

---
+++ Easy/1005-Maximize Sum Of Array After K Negations.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int largestSumAfterKNegations(vector<int>& A, int K) {
+        
+        sort(A.begin(), A.end());
+        
+        for(int i=0; K>0 && A[i]<0 && i<A.size();i++,K--){
+            A[i] = -A[i];
+        }
+        
+        return accumulate(A.begin(), A.end(),0) - K%2 * *min_element(A.begin(),A.end()) * 2;
+        
+    }
+};
