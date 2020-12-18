c22ce1c70d6f91ab346774756e979d6b49e5dc17
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2020-12-18 17:18:59 +0530

    array

---
+++ Medium/334-Increasing Triplet Subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        
+        int c1 = INT_MAX;
+        int c2 = INT_MAX;
+        
+        for(auto x:nums){
+            if(x <= c1){
+                c1 = x;
+            }else if(x <= c2){
+                c2 = x;
+            }else{
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
