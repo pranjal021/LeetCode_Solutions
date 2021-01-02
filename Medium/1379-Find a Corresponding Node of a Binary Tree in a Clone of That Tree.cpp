331f943c7a6c57ac692062d96471f301a24264e8
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2021-01-02 21:12:32 +0530

    tree | dfs | bfs | recursion

---
+++ Medium/1379-Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        
+        if(original == NULL || target == NULL) return NULL;
+        
+        if(original == target) return cloned;
+        
+        auto left = getTargetCopy(original->left,cloned->left,target);
+        
+        return left ? left : getTargetCopy(original->right, cloned->right,target);
+        
+    }
+};
