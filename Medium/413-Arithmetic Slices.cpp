69e6b44ba4afa0a6a0680c2ae0db841f0628e477
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2021-02-18 14:28:00 +0530

    maths | dp

---
+++ Medium/413-Arithmetic Slices.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& A) {
+        
+        int n = A.size();
+        if(n < 3) return 0;
+        vector<int> dp(n,0);
+        
+        if(A[2]-A[1] == A[1]-A[0]) dp[2] = 1;
+        
+        int result = dp[2];
+        
+        for(int i=3;i<n;i++){
+            if(A[i]-A[i-1] == A[i-1]-A[i-2])
+                dp[i] = dp[i-1]+1;
+            result += dp[i];
+        }
+        
+        return result;
+    }
+};
