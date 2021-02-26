3dc33464af08717e77024cf9408ba186eae732d4
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2021-02-26 15:39:31 +0530

    bfs | dfs | graph

--- Medium/133-Clone Graph.cpp
+++ Medium/133-Clone Graph.cpp
@@ -1,4 +1,4 @@
-
+// 1
 class Solution {
 public:
     
@@ -27,6 +27,69 @@ public:
         
         unordered_map<Node*,Node*> m;
         return helper(node,m);
+        
+    }
+};
+
+//2 : bfs 
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        
+        if(!node){
+            return NULL;
+        }
+        
+        Node* copy = new Node(node->val);
+        unordered_map<Node*, Node*> visited;
+        
+        queue<Node*> q;
+        q.push(node);
+        visited[node] = copy;
+        
+        while(!q.empty()){
+            
+            auto f = q.front();
+            q.pop();
+            
+            for(auto nbr : f->neighbors){
+                if(!visited[nbr]){
+                    visited[nbr] = new Node(nbr->val);
+                    q.push(nbr);
+                }
+                visited[f]->neighbors.push_back(visited[nbr]);
+            }
+        }        
+        
+        return visited[node];
+    }
+};
+
+//3. dfs
+Node* dfs_helper(Node* node, unordered_map<Node*,Node*> &v){
+    
+    if(!node) return node;
+    
+    if(v[node]) return v[node];
+    
+    Node* copy = new Node(node->val);
+    v[node] = copy;
+    
+    for(auto nbr : node->neighbors){
+        copy->neighbors.push_back(dfs_helper(nbr,v));
+    }
+    
+    return copy;
+}
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        
+        if(!node) return NULL;
+        
+        unordered_map<Node*,Node*> v;
+        return dfs_helper(node, v);
         
     }
 };
