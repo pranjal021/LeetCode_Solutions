23be555e147d899c5448bc5c32dc092bfda72c2b
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2021-02-28 20:01:55 +0530

    hash table

--- Easy/463-Island Perimeter.cpp
+++ Easy/463-Island Perimeter.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int count=0;
-        int repeat = 0;
+        
+        int ans = 0;
         int n = grid.size();
         int m = grid[0].size();
+        
+        if(n == 0|| m == 0) return ans;
+        
+        vector<vector<int>> dir{{1,0}, {0,-1}, {-1,0}, {0,1}};
+        
+        int count = 0;
+        int nbrs = 0;
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 
                 if(grid[i][j] == 1){
-                    count ++;
-                    if(i != 0) if(grid[i-1][j] == 1) repeat++;
-                    if(j != 0) if(grid[i][j-1] == 1) repeat++;
-                }
+                    count++;
                     
+                    for(int d=0;d<dir.size();d++){
+                        int r = i + dir[d][0];
+                        int c = j + dir[d][1];
+                        
+                        if(r>=0 && c>=0 && r<n && c<m && grid[r][c] == 1){
+                            nbrs++;
+                        }
+                    }
+                }
+                
             }
         }
         
-        return count*4 - repeat*2;
+        ans = 4 * count - nbrs;
+        return ans;
     }
 };
