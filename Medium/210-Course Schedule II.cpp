c4b20d327dc0bc600abc8120b4061d66bf55e644
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2021-03-01 16:09:37 +0530

    bfs | dfs | graph | topological sort

--- Medium/210-Course Schedule II.cpp
+++ Medium/210-Course Schedule II.cpp
@@ -50,4 +50,64 @@ public:
     }
 };
 
+//bfs:
 
+class Solution {
+public:
+    
+    vector<int> getDegree(int n, vector<vector<int>>& graph){
+        
+        vector<int> indegree(n);
+        
+        for(int i=0;i<n;i++){
+            for(auto val : graph[i]){
+                indegree[val]++;
+            }
+        }
+        
+        return indegree;
+    }
+    
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        //graph
+        vector<vector<int>> graph(numCourses);
+        
+        for(auto v : prerequisites){
+            graph[v[0]].push_back(v[1]);
+        }
+        //indegree
+        vector<int> indegree = getDegree(numCourses,graph);
+        
+        vector<int> ans;
+        queue<int> q;
+        
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        while(!q.empty()){
+            int f = q.front();
+            q.pop();
+            
+            ans.push_back(f);
+            
+            for(auto v : graph[f]){
+                if(--indegree[v] == 0){
+                    q.push(v);
+                }
+            }
+            
+        }
+        
+        if(ans.size() == numCourses){
+            reverse(ans.begin(), ans.end());
+        }else{
+            ans.clear();
+        }
+        
+        return ans;
+    }
+};
