c711901a10be6e125294a872ba8a2d471bbab410
pranjal mittal <53166430+pranjal021@users.noreply.github.com>
2021-05-11 17:21:09 +0530

    stack

--- Easy/496-Next Greater Element I.cpp
+++ Easy/496-Next Greater Element I.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
+    
+    vector<int> find(vector<int> &arr){
+        
+        stack<int> s;
+        vector<int> ans(arr.size(),0);
+        
+        for(int i = arr.size() - 1 ; i >= 0 ; i--){
+            while(s.size() > 0 && s.top() < arr[i]) s.pop();
+            ans[i] = s.size() > 0 ? s.top() : -1;
+            s.push(arr[i]);
+        }
+        
+        return ans;
+    }
+    
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         
-        vector<int> res;
+        vector<int> nge = find(nums2);
+        
         unordered_map<int,int> m;
         
         for(int i=0;i<nums2.size();i++){
-            m[nums2[i]] = i;
+            m[nums2[i]] = nge[i];
         }
+        
+        vector<int> ans(nums1.size());
         
         for(int i=0;i<nums1.size();i++){
-            int curr = -1;
-            int j = m[nums1[i]] + 1;
-            
-            for(; j < nums2.size() ; j++){
-                if(nums2[j] > nums1[i]){
-                    curr = nums2[j];
-                    break;
-                }
-            }
-            
-            res.push_back(curr);
+            ans[i] = m[nums1[i]];
         }
         
-        return res;
+        return ans;
     }
 };
+
